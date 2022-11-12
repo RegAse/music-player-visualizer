ca0de74e8144f7dc62c23cad91ee4c23aa97b819
RegAse <gudmundur210@gmail.com>
2022-11-12 02:44:40 +0000

    Fully implemented play and pause

--- src/App.js
+++ src/App.js
@@ -14,14 +14,32 @@ function App() {
   let songs = data["songs"];
 
   const [currentSong, setCurrentSong] = useState();
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    Visualizer().onChange();
-  });
+    if(isPlaying) {
+      Visualizer().onChange();
+    }
+  }, [currentSong]);
 
   function handleClick(e) {
     console.log("Clicked on " + e.currentTarget.dataset.id);
     setCurrentSong(songs[e.currentTarget.dataset.id]);
+    setIsPlaying(true);
+  }
+
+  function handlePauseClick() {
+    let audio = document.getElementById("audio");
+    audio.pause();
+    setIsPlaying(false);
+    console.log("pause");
+  }
+
+  function handlePlayClick() {
+    let audio = document.getElementById("audio");
+    audio.play();
+    setIsPlaying(true);
+    console.log("Play");
   }
 
   return (
@@ -48,12 +66,16 @@ function App() {
           <div className='col-md-7'>
             <div id='canvas-container'>
               <div className='overlay'>
-                <BsFillPauseFill></BsFillPauseFill>
+                {isPlaying ?
+                  <BsFillPauseFill onClick={handlePauseClick}></BsFillPauseFill>
+                  :
+                  <BsFillPlayFill onClick={handlePlayClick}></BsFillPlayFill>
+                }
                 {/* <h1><BsFillPlayFill></BsFillPlayFill></h1> */}
               </div>
               {currentSong ?
                 <div>
-                  {currentSong.title}
+                  {/* {currentSong.title} */}
                   <audio id="audio" src={currentSong.file}></audio>
                 </div>
                 :
